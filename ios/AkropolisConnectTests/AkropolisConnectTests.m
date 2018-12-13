5aca3c6279a5d70fe2ac13bb2431a3f3234d503f
Дмитрий Хурсевич <varimartas@yandex.ru>
2018-12-13 23:59:18 +0700

    add native configuration for ios

--- ios/AkropolisConnectTests/AkropolisConnectTests.m
+++ ios/AkropolisConnectTests/AkropolisConnectTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AkropolisConnectTests : XCTestCase
+@interface akropolisconnectTests : XCTestCase
 
 @end
 
-@implementation AkropolisConnectTests
+@implementation akropolisconnectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
